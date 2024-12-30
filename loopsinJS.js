bad6a81c687e03686fcea2d22f019d65a19ade51
Sarmad Khan <sarmadarbabkhan123@gmail.com>
2024-12-30 12:06:18 +0500

    Add files via upload

---
+++ loopsinJS.js
@@ -0,0 +1,20 @@
+const obj = [
+    { id:1,
+        file:'Incubation',
+        promt:'Just type the text in google search'
+    },
+    {
+        id:2,
+        file:'MERN Stack',
+        promt:'Full-Stack'
+    },
+    {
+        id:34,
+        file:'SK',
+        promt:'Aj'
+    }
+];
+for(let i=0; i<obj.length; i++){
+    console.log(obj[i].file);
+    console.log(obj[i].promt);
+}
